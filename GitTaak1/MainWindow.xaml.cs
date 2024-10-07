d97f67cbfba66e5957b6a38cafd1f797a35199d3
AhmedUlusoyPXL <116811465+AhmedUlusoyPXL@users.noreply.github.com>
2024-10-08 00:00:50 +0200

    Add files via upload
    Kon niet clonen bleef erros krigen

---
+++ GitTaak1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitTaak1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void HoverImage(object sender, MouseEventArgs e)
+        {
+            if (e.RoutedEvent == MouseEnterEvent) 
+            {
+                mainGrid.Background = new SolidColorBrush(Colors.White); 
+            }
+            else
+            {
+                mainGrid.Background = new SolidColorBrush(Colors.LightGray);
+            }
+        }
+
+        private void SelectedImage(object sender, MouseEventArgs e)
+        {
+            Image img = sender as Image;
+            switch (img.Name)
+            {
+                case "imgRood":
+                    txtCode.Text = "#FF0000";
+                    txtInformatie.Text = "Rood is de kleur van warmte";
+                    break;
+                case "imgGroen":
+                    txtCode.Text = "#008000";
+                    txtInformatie.Text = "Groen is de kleur van genezing";
+                    break;
+                case "imgGeel":
+                    txtCode.Text = "#FFFF00";
+                    txtInformatie.Text = "Geel is de kleur van levenslust";
+                    break;
+                case "imgBlauw":
+                    txtCode.Text = "#0000FF";
+                    txtInformatie.Text = "Blauw is de kleur van intelligentie";
+                    break;
+            }
+        }
+    }
+}
